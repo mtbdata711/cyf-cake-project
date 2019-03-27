f94392c850b27d01d70fe1de51ea13a319d54ad2
mtbdata711 <bengethomasmatthew@gmail.com>
2019-03-27 21:47:26 +0000

    first hw attempt

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function imageCarouselEvent() {
+  var secondaryImages = document.getElementById("secondary-image");
+  secondaryImages.addEventListener("click", function(e) {
+    if (e.target.src) {
+      swapFeaturedImage(e.target);
+    }
+  });
+}
+
+function swapFeaturedImage(selectedElement) {
+  var featuredImage = document.querySelector(".primary-image");
+  if (selectedElement.src) {
+    var newFeaturedImage = selectedElement.src;
+    //Move current primary-image to secondary-image
+    selectedElement.src = featuredImage.src;
+    //Move new primary-image to primary-image div
+    featuredImage.src = newFeaturedImage;
+  }
+}
+imageCarouselEvent();
